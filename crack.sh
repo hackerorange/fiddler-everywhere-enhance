cbb650a5209ef618779450744a2001241cf6ef12
zhongct <zhongct@glodon.com>
2023-05-28 11:11:50 +0800

    submit

--- crack.sh
+++ crack.sh
@@ -4,12 +4,6 @@ root_dir=$(cd `dirname $0` && pwd -P)
 
 export PATH="~/test/bin/net6.0/osx-x64:$PATH"
 
-dll_file_webui="/mnt/disk2/fiddler3.2.0/resources/app/out/WebServer/Fiddler.WebUi.dll"
-# ildasm -tok -byt $dll_file_webui -out=Fiddler.WebUi.il
-# ilasm -dll -X64 -output=$dll_file_webui Fiddler.WebUi.il
-ilasm -dll -arm64 -output=Fiddler.WebUi.dll Fiddler.WebUi.il
+# ilasm -dll -arm64 -output=Fiddler.WebUi.dll Fiddler.WebUi.il
 
-dll_file_sdk="/mnt/disk2/fiddler3.2.0/resources/app/out/WebServer/FiddlerBackendSDK.dll"
-# ildasm -tok -byt $dll_file_sdk -out=FiddlerBackendSDK.il
-# ilasm -dll -X64 -output=$dll_file_sdk FiddlerBackendSDK.il
 ilasm -dll -arm64 -output=FiddlerBackendSDK.dll FiddlerBackendSDK.il
